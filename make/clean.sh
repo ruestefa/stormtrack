56e751612071f3f8caa0a0b280b093d88dbb90b2
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-02-17 16:04:30 +0100

    turn clean.sh into a proper script and rename the others

--- make/clean.sh
+++ make/clean.sh
@@ -1,48 +1,86 @@
 #!/bin/bash
 
-echo "error: script not tested yet" >&2
-exit 1
+
+cli()
+{
+    local actions=(${@})
+    possible_actions=(python cython pyc build test all)
+    usage="$(basename ${0}) ACTION[S]\nACTIONS: ${possible_actions[@]}"
+    [ "${#actions[@]}" -eq 0 ] && { echo -e "${usage}" >&2; return 1; }
+    calls=()
+    for action in "${actions[@]}"; do
+        case "${action}" in
+            python) calls+=(clean_python);;
+            cython) calls+=(clean_cython);;
+            pyc) calls+=(clean_pyc);;
+            build) calls+=(clean_build);;
+            test) calls+=(clean_test);;
+            all) calls+=(clean_all);;
+            *) echo -e "error: invalid action '${action}'\n${usage}" >&2; return 1;;
+        esac
+    done
+    for call in "${calls[@]}"; do
+        "${call}" || return
+    done
+}
 
 
 clean_python()
 {
-    echo -e "\n[make clean-python] removing Python build artifacts"
-    \rm -rf "build/"
-    \rm -rf "dist/"
-    \rm -rf ".eggs/"
-    \rm -ff $(\find . -not -path './venv*' -and -not -path './ENV*' -name '*.egg' -exec echo "rm -ff '{}'" \;)
-    \rm -ff $(\find . -not -path './venv*' -and -not -path './ENV*' -name '*.egg' -exec echo "rm -ff '{}'" \;)
+    echo -e "[clean_python] removing Python build artifacts"
+    \rm -vrf "build/"
+    \rm -vrf "dist/"
+    \rm -vrf ".eggs/"
+    \find . -not -path './venv*' -and -not -path './ENV*' -name '*.egg' -exec \rm -vrf '{}' \+
+    \find . -not -path './venv*' -and -not -path './ENV*' -name '*.egg-info' -exec \rm -vrf '{}' \+
 }
 
 
 clean_cython()
 {
-    echo -e "\n[make clean-cython] removing Cython build artifacts"
-    \find src -name '*.html' -exec rm -f {} \+
-    \find src -name '*.c' -exec rm -f {} \+
-    \find src -name '*.so' -exec rm -f {} \+
+    echo -e "[clean_cython] removing Cython build artifacts"
+    \find src -name '*.html' -exec rm -vf {} \+
+    \find src -name '*.c' -exec rm -vf {} \+
+    \find src -name '*.so' -exec rm -vf {} \+
 }
 
 
 clean_pyc()
 {
-    echo -e "\n[make clean-pyc] removing Python file artifacts"
-    \rm -rf $(\find . -not -path './venv*' -and -not -path './ENV*' -name '*.pyc'       -exec echo "rm -rf '{}'" \;)
-    \rm -rf $(\find . -not -path './venv*' -and -not -path './ENV*' -name '*.pyo'       -exec echo "rm -rf '{}'" \;)
-    \rm -rf $(\find . -not -path './venv*' -and -not -path './ENV*' -name '*~'          -exec echo "rm -rf '{}'" \;)
-    \rm -rf $(\find . -not -path './venv*' -and -not -path './ENV*' -name '__pycache__' -exec echo "rm -rf '{}'" \;)
+    echo -e "[clean_pyc] removing Python file artifacts"
+    \find . -not -path './venv*' -and -not -path './ENV*' -name '*.pyc'       -exec \rm -vrf '{}' \+
+    \find . -not -path './venv*' -and -not -path './ENV*' -name '*.pyo'       -exec \rm -vrf '{}' \+
+    \find . -not -path './venv*' -and -not -path './ENV*' -name '*~'          -exec \rm -vrf '{}' \+
+    \find . -not -path './venv*' -and -not -path './ENV*' -name '__pycache__' -exec \rm -vrf '{}' \+
+}
+
+
+clean_build()
+{
+    echo -e "[clean_build] removing all build artifacts"
+    clean_python || { echo "error in clean_python" >&2; return 1; }
+    clean_cython || { echo "error in clean_cython" >&2; return 1; }
+    clean_pyc || { echo "error in clean_pyc" >&2; return 1; }
 }
 
 
 clean_test()
 {
-    echo -e "\n[make clean-test] removing testing artifacts"
-    \rm -rf ".tox/"
-    \rm -f ".coverage"
-    \rm -rf "htmlcov/"
-    \rm -rf ".pytest_cache"
-    \rm -rf ".mypy_cache"
+    echo -e "[clean_test] removing testing artifacts"
+    \rm -vrf ".tox/"
+    \rm -vf ".coverage"
+    \rm -vrf "htmlcov/"
+    \rm -vrf ".pytest_cache"
+    \rm -vrf ".mypy_cache"
 }
 
 
-# TODO
+clean_all()
+{
+    echo -e "[clean_all] clean up everything"
+    clean_build || { echo "error in clean_build" >&2; return 1; }
+    clean_test || { echo "error in clean_test" >&2; return 1; }
+}
+
+
+cli "${@}"
